c7821450e83d93e491c1ffd491b25bdd07262357
Gunjan6788 <gunjan6788@gmail.com>
2020-06-12 19:59:54 +0530

    sql_queries

--- queries.sql
+++ queries.sql
@@ -15,7 +15,20 @@ CREATE TABLE user(id INT NOT NULL AUTO_INCREMENT,
     -> email VARCHAR(20),
     -> dob DATE,
     -> username VARCHAR(20),
+    -> status VARCHAR(20)
     -> password VARCHAR(20),
     -> PRIMARY KEY(id));
 
+create table category(id INT NOT NULL AUTO_INCREMENT,
+    -> category VARCHAR(30),
+    -> PRIMARY KEY(id)
+    -> );
 
+create table ticket(id INT NOT NULL AUTO_INCREMENT, title VARCHAR(200),
+    -> description VARCHAR(300),
+    -> user_id INT,
+    -> category_id INT,
+    -> PRIMARY KEY(id)
+    -> FOREIGN KEY(user_id) REFERENCES user(id),
+    -> FOREIGN KEY(category_id) REFERENCES category(id)
+    -> );
