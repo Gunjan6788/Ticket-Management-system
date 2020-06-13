7cb98769d13dfef97b6681bb22fa1216a6536c23
Gunjan6788 <gunjan6788@gmail.com>
2020-06-13 11:07:37 +0530

    action added

--- ticket_management/src/Redux/reducer.js
+++ ticket_management/src/Redux/reducer.js
@@ -1,8 +1,37 @@
+import {
+    USER_REGISTER,
+    COMPANY_REGISTER,
+    LOGIN
+} from './actionTypes'
 
 export const initState = {
     data: [],
+    userSignup: false,
+    companySignup: false,
+    loginData: []
 }
 
 export default (state = initState, { type, payload }) => {
-
+    console.log(payload)
+    switch (type) {
+        case USER_REGISTER:
+            alert(payload.data)
+            return {
+                ...state,
+                userSignup: true
+            }
+        case COMPANY_REGISTER:
+            alert(payload.data)
+            return {
+                ...state,
+                companySignup: true,
+            }
+        case LOGIN:
+            return {
+                ...state,
+                loginData: payload
+            }
+        default:
+            return state
+    }
 }
